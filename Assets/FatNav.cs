33958e022b8ce10a01e520e88337477369853b98
Terrence <warhorse+meowstrom@gmail.com>
2016-04-20 22:58:16 -0400

    add a fat cat

---
+++ Assets/FatNav.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FatNav : Cat
+{
+
+    // Use this for initialization
+    public override void Start()
+    {
+        rate = 0.0f;
+        staminaRegen = 0.25f;
+        staminaLoss = 0.05f;
+        baseSpeed = 6.0f;
+        baseAccel = 25.0f;
+        speedModifier = (float)Random.Range(0.7f, 1.3f);
+
+
+        setInit();
+    }
+
+
+}
